3d924b2afba65b96758f6484e6fc8e714ca10cae
sanikaghule16 <sanikaghule4@gmail.com>
2026-04-01 12:30:19 +0530

    Add WebForm1 with basic form submission logic

---
+++ Webform1.aspx.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace validator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                lblMsg.Text = "Form submitted successfully!";
+            }
+        }
+    }
+}
